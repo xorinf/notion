7e27aba34c5d939a0cf37744155ee103fe191f6f
FriendAssKicker <118471720+FriendAssKicker@users.noreply.github.com>
2026-05-13 13:30:45 +0530

    added: auto startup feature added

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -2,3 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 osascript -e 'tell app "Terminal" to do script "cd '$DIR'/../frontend && npm run dev"'
 osascript -e 'tell app "Terminal" to do script "cd '$DIR'/../backend && nodemon server.js"'
+open http://localhost:5173
